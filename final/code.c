de6e5a857377d448d599176432e4b50219a7bc60
ROSHAN0120 <roshanbinu2002@gmail.com>
2023-06-03 13:25:25 +0530

    new

--- final/code.c
+++ final/code.c
@@ -10,11 +10,11 @@ void delay(int);
 void lcdinit(void);
 void writecmd(int);
 void writedata(char);
-void Return(void);
-char READ_SWITCHES(void);
-char get_key(void);
-unsigned int count = 0;
-unsigned char val = 0;
+
+unsigned char READ_SWITCHES(void);
+unsigned char get_key(void);
+unsigned char count = 0;
+
 //*******************
 //Pin description
 /*
@@ -27,54 +27,61 @@ P1.4 to P1.7 are keypad column inputs
 //********************
 // Define Pins
 //********************
-sbit RowA = P1^0;     //RowA
-sbit RowB = P1^1;     //RowB
-sbit RowC = P1^2;     //RowC
-sbit RowD = P1^3;     //RowD
+sbit RowA = P2^0;     //RowA
+sbit RowB = P2^1;     //RowB
+sbit RowC = P2^2;     //RowC
+sbit RowD = P2^3;     //RowD
 
-sbit C1   = P1^4;     //Column1
-sbit C2   = P1^5;     //Column2
-sbit C3   = P1^6;     //Column3
-sbit C4   = P1^7;     //Column4
+sbit C1   = P2^4;     //Column1
+sbit C2   = P2^5;     //Column2
+sbit C3   = P2^6;     //Column3
+sbit C4   = P2^7;     //Column4
 
-sbit E    = P3^6;     //E pin for LCD
-sbit RS   = P3^7;     //RS pin for LCD
+sbit RS    = P3^6;     //E pin for LCD
+sbit E   = P3^7;     //RS pin for LCD
 
 
 int main(void)
 {
-   char key;                // key char for keeping record of pressed key
+   unsigned char key;                // key char for keeping record of pressed key
 
    cct_init();              // Make input and output pins as required
    lcdinit();               // Initilize LCD
-
+	TMOD = 0x20;
+	TH1 = 0xFD;
+	SCON = 0x50;
+	TR1 =1;
    while(1)
    { 
      key = get_key();       // Get pressed key
-	        
-	 writedata(key);        // Echo the key pressed to LCD
+	writecmd(0x01);        // Clear screen    
+	writedata(key);        // Echo the key pressed to LCD
+	SBUF =key;
+	while (T1 == 0); 
+	T1 = 0; 
    }
 }
 
 
 void cct_init(void)
 {
-	P0 = 0x00;   //not used
-	P1 = 0xf0;   //used for generating outputs and taking inputs from Keypad
-	P2 = 0x00;   //used as data port for LCD
+	P2 = 0xf0;   //used for generating outputs and taking inputs from Keypad
+	P0 = 0x00;   //used as data port for LCD
 	P3 = 0x00;   //used for RS and E   
 }
 
-void delay(int a)
+
+
+void delay( int a)
 {
    int i;
    for(i=0;i<a;i++);   //null statement
 }
 
-void writedata(char t)
+void writedata(unsigned char t)
 {
    RS = 1;             // This is data
-   P2 = t;             //Data transfer
+   P0 = t;             //Data transfer
    E  = 1;             // => E = 1
    delay(150);
    E  = 0;             // => E = 0
@@ -85,7 +92,7 @@ void writedata(char t)
 void writecmd(int z)
 {
    RS = 0;             // This is command
-   P2 = z;             //Data transfer
+   P0 = z;             //Data transfer
    E  = 1;             // => E = 1
    delay(150);
    E  = 0;             // => E = 0
@@ -98,64 +105,45 @@ void lcdinit(void)
      delay(15000);
    writecmd(0x30);
      delay(4500);
-   writecmd(0x30);
-     delay(300);
-   writecmd(0x30);
-     delay(650);
-  
+ 
    writecmd(0x38);    //function set
    writecmd(0x0c);    //display on,cursor off,blink off
    writecmd(0x01);    //clear display
    writecmd(0x06);    //entry mode, set increment
 }
 
-void Return(void)     //Return to 0 location on LCD
-{
-  writecmd(0x02);
-    delay(1500);
-}
+//void Return(void)     //Return to 0 location on LCD
+//{
+  //writecmd(0x02);
+    //delay(1500);
+//}
 
 
 
 
-char READ_SWITCHES(void)	
+unsigned char READ_SWITCHES(void)	
 {	
 	RowA = 0; RowB = 1; RowC = 1; RowD = 1; 	
+	
+
    
 	if (C1 == 0) 
    { 
 		   count = 0 ;
-      count=count+1;
+      
       delay(10000);
 		 	if (C1 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
+		
 
      
 				switch (count)
 			 {
-				case 2: return '7';break;
-				case 3 : return 'A';break;
-				case 4 : return 'A';break;
-				case 5 : return 'B';break;
+				case 0: return '7';break;
+				case 1 : return 'A';break;
 					default :return 'B';break;
 			 }				
 		 
@@ -169,37 +157,22 @@ char READ_SWITCHES(void)
 	
 	{ 
 		   count = 0 ;
-      count=count+1;
+  
       delay(10000);
 		 	if (C2 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
+			
 
      
 				switch (count)
 			 {
-				case 2: return '8';break;
-				case 3 : return 'C';break;
-				case 4 : return 'C';break;
-				case 5 : return 'D';break;
+				case 0: return '8';break;
+				case 1 : return 'C';break;
+				
+	
 				default :return 'D';break;
 			 }				
 		 
@@ -214,37 +187,22 @@ char READ_SWITCHES(void)
 	
    { 
 		   count = 0 ;
-      count=count+1;
+ 
       delay(10000);
 		 	if (C3 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
+			
 
      
 				switch (count)
 			 {
-				case 2: return '9';break;
-				case 3 : return 'E';break;
-				case 4 : return 'E';break;
-				case 5 : return 'F';break;
+				case 0: return '9';break;
+				case 1 : return 'E';break;
+				
+				
 				default :return 'F';break;
 			 }				
 		 
@@ -258,37 +216,21 @@ char READ_SWITCHES(void)
 	
  { 
 		   count = 0 ;
-      count=count+1;
+  
       delay(10000);
 		 	if (C4 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
-
+	
      
 				switch (count)
 			 {
-				case 2: return '/';break;
-				case 3 : return 'G';break;
-				case 4 : return 'G';break;
-				case 5 : return 'H';break;
+				case 0: return '/';break;
+				case 1 : return 'G';break;
+				
+			
 				default :return 'H';break;
 			 }				
 		 
@@ -300,37 +242,20 @@ char READ_SWITCHES(void)
 	if (C1 == 0) 
    { 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C1 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
+			
 
-
-     
 				switch (count)
 			 {
-				case 2: return '4';break;
-				case 3 : return 'I';break;
-				case 4 : return 'I';break;
-				case 5 : return 'J';break;
+				case 0: return '4';break;
+				case 1 : return 'I';break;
+
 					default :return 'J';break;
 			 }				
 		 
@@ -344,38 +269,22 @@ char READ_SWITCHES(void)
 	
 	{ 
 		   count = 0 ;
-      count=count+1;
+   
       delay(10000);
 		 	if (C2 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
+	
 
      
 				switch (count)
 			 {
-				case 2: return '5';break;
-				case 3 : return 'K';break;
-				case 4 : return 'K';break;
-				case 5 : return 'L';break;
-				default :return 'L';break;
+				case 0: return '5';break;
+				case 1 : return 'K';break;
+				
+						default :return 'L';break;
 			 }				
 		 
    }
@@ -387,80 +296,47 @@ char READ_SWITCHES(void)
 	
    { 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C3 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
-
+		
+		
      
 				switch (count)
 			 {
-				case 2: return '6';break;
-				case 3 : return 'M';break;
-				case 4 : return 'M';break;
-				case 5 : return 'N';break;
+				case 0: return '6';break;
+				case 1 : return 'M';break;
+			
 				default :return 'N';break;
 			 }				
 		 
    }
 
-	if (C4 == 0) 
+
    if (C4 == 0) 
 		
 	
 	
  { 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C4 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
-
+		
      
 				switch (count)
 			 {
-				case 2: return '*';break;
-				case 3 : return 'O';break;
-				case 4 : return 'O';break;
-				case 5 : return 'P';break;
+				case 0: return '*';break;
+				case 1 : return 'O';break;
+
 				default :return 'P';break;
 			 }				
 		 
@@ -472,37 +348,21 @@ char READ_SWITCHES(void)
 	 if (C1 == 0) 
    { 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C1 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C1 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
-
+	
+		
      
 				switch (count)
 			 {
-				case 2: return '1';break;
-				case 3 : return 'Q';break;
-				case 4 : return 'Q';break;
-				case 5 : return 'R';break;
+				case 0: return '1';break;
+				case 1 : return 'Q';break;
+
 					default :return 'R';break;
 			 }				
 		 
@@ -516,37 +376,20 @@ char READ_SWITCHES(void)
 	
 	{ 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C2 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
-
-     
+	
+			
 				switch (count)
 			 {
-				case 2: return '2';break;
-				case 3 : return 'S';break;
-				case 4 : return 'S';break;
-				case 5 : return 'T';break;
+				case 0: return '2';break;
+				case 1 : return 'S';break;
+
 				default :return 'T';break;
 			 }				
 		 
@@ -559,80 +402,49 @@ char READ_SWITCHES(void)
 	
    { 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C3 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C3 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
+		
+	
 
      
 				switch (count)
 			 {
-				case 2: return '3';break;
-				case 3 : return 'U';break;
-				case 4 : return 'U';break;
-				case 5 : return 'V';break;
+				case 0: return '3';break;
+				case 1 : return 'U';break;
+
 				default :return 'V';break;
 			 }				
 		 
    }
 
-	if (C4 == 0) 
+	
    if (C4 == 0) 
 		
 	
 	
  { 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C4 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C4 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
 
-
+	
      
 				switch (count)
 			 {
-				case 2: return '-';break;
-				case 3 : return 'W';break;
-				case 4 : return 'W';break;
-				case 5 : return 'X';break;
+				case 0: return '-';break;
+				case 1 : return 'W';break;
+
 				default :return 'X';break;
 			 }				
 		 
@@ -649,37 +461,21 @@ char READ_SWITCHES(void)
 	
 	{ 
 		   count = 0 ;
-      count=count+1;
+
       delay(10000);
 		 	if (C2 == 0)
 			{				
 		     count=count+1;
 		    delay(10000);
 			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-			if (C2 == 0) 
-			{
-				count=count+1;
-				delay(10000);
-			}
-
+			
 
      
 				switch (count)
 			 {
-				case 2: return '0';break;
-				case 3 : return 'Y';break;
-				case 4 : return 'Y';break;
-				case 5 : return 'Z';break;
+				case 0: return '0';break;
+				case 1 : return 'Y';break;
+
 				default :return 'Z';break;
 			 }				
 		 
@@ -693,7 +489,7 @@ char READ_SWITCHES(void)
 	return 'n';           	// Means no key has been pressed
 }
 
-char get_key(void)           //get key from user
+unsigned char get_key(void)           //get key from user
 {
 	char key = 'n';              //assume no key pressed
 
